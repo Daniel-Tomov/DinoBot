1c18219303b0cab52101ae514ff1baa5f9ea56c2
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2024-02-20 01:12:06 -0500

    fix: time test

--- rssparser.py
+++ rssparser.py
@@ -15,14 +15,19 @@ class RSSParser:
     # send RSS updates
     @tasks.loop(hours = 1)
     async def rssSendMessage(self):
+        await self.client.wait_until_ready()
+        channel = self.client.get_channel(1208824287457321080)
+        await channel.send("Checking for RSS")
+
         feed = feedparser.parse(self.url)
         now = datetime.now().astimezone()
         time_range = timedelta(hours = 1)
 
         for entry in feed.entries:
             entry_time = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %z").astimezone()
+            #print(f'now: {now}\t entry time: {entry_time}\t time diff: {now - entry_time}\t time range: {time_range}')
 
             if now - entry_time <= time_range:
                 discMessageString = f'Title: {entry.title}\nPublication Date: {entry.published}\nSummary: {entry.summary}'
-                channel = await self.client.get_channel(1208824287457321080)
+                channel = self.client.get_channel(1208824287457321080)
                 await channel.send(discMessageString)
